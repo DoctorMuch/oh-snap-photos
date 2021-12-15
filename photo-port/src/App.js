266e750c0d13818de3c2bc34db66b489b578bb3f
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-12-15 14:53:31 -0600

    form with front-end submit handler

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -29,6 +29,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
