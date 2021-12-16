31d67d40c074fc8005d42c4a7dad93d4f54bf4c0
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-12-16 13:01:03 -0600

    modal image renders after adding .default

--- photo-port/src/components/PhotoList/index.js
+++ photo-port/src/components/PhotoList/index.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
-  const [currentPhoto, setCurrentPhoto] = useState();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [photos] = useState([
     {
@@ -103,14 +102,17 @@ const PhotoList = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
+  const [currentPhoto, setCurrentPhoto] = useState();
+
   const toggleModal = (image, i) => {
     setCurrentPhoto({...image, index: i})
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div>
-     {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen &&
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
